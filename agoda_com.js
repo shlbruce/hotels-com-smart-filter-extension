622ae08fdd5d714ce3a10284eb53eda27942be90
shlbruce <shlbruce@hotmail.com>
2025-06-17 14:50:53 -0500

    add basic framework

--- agoda_com.js
+++ agoda_com.js
@@ -40,8 +40,399 @@ function addSmartFilterButtonOnAgodaCom() {
     button.addEventListener("mouseleave", resetButtonStyle);
 
     // Your smart filter logic
-    button.addEventListener("click", handleSmartFilterClickOnHotelsCom);
+    button.addEventListener("click", handleSmartFilterClickOnAgodaCom);
 
     textSearch.insertAdjacentElement("afterend", button);
+}
+
+function handleSmartFilterClickOnAgodaCom(event) {
+
+    event.stopPropagation(); // ⛔ Prevents event bubbling
+    event.preventDefault();  // ⛔ Prevents default action
+
+    chrome.storage.sync.get(["smartFilters"], function (result) {
+        const smartFilters = result.smartFilters;
+        if (smartFilters) {
+            const clearButton = document.querySelector(span[label="CLEAR"]);
+            if (clearButton) {
+                clearButton.click();
+            }
+
+            setTimeout(() => {
+                applyFiltersInAgodaCom(smartFilters);
+            }, 2000);
+
+        } else {
+            console.warn("No smart filters found in storage.");
+        }
+    });
+}
+
+const AGODA_COM_MAP = {
+    // Room Accessibility
+    roll_in_shower: "Roll-in shower",
+    in_room: "In-room accessibility",
+    accessible_bathroom: "Accessible bathroom",
+
+
+    // Property Accessibility
+    elevator: "Elevator",
+    service_animals: "Service animals allowed",
+    stair_free: "Stair-free path to entrance",
+    wheelchair_parking: "Wheelchair accessible parking",
+    sign_language: "Sign language-capable staff",
+
+    //
+    // Property Amenities
+    //
+    airport_shuttle: "Airport shuttle included",
+    bar: "Bar",
+    casino: "Casino",
+    electric_charger: "Electric car charging station",
+    golf_course: "Golf course",
+    gym: "Gym",
+    parking: "Parking",
+    pool: "Pool",
+    restaurant: "Restaurant",
+    spa: "Spa",
+    washer_dryer: "Washer and dryer",
+    water_park: "Water park",
+
+    // Room amenities
+    air_conditioned: "Air conditioned",
+    cribs: "Cribs",
+    hot_tub: "Hot tub",
+    kitchen: "Kitchen",
+    ocean_view: "Ocean view",
+    outdoor_space: "Outdoor space",
+    wifi: "Wifi Included",
+
+    // Availability
+    available_only: "Only show available properties",
+
+    // Cancellation
+    fully_refundable: "Fully refundable property",
+
+    // Discounts
+    vip: "VIP Access properties",
+    member: "Member Prices",
+    discounted: "Discounted properties",
+
+    // Guest Rating
+    guest_rating_any: "Any",
+    guest_rating_9: "Wonderful 9+",
+    guest_rating_8: "Very good 8+",
+    guest_rating_7: "Good 7+",
+
+    // Payment Flexibility
+    pay_later: "Reserve now, pay later",
+    hotels_com_gift_card: "Pay with Hotels.com gift card",
+
+
+    // Property Type
+    hotel: "Hotel",
+    resort: "Resort",
+    bnb: "Bed & breakfast",
+    condo: "Condo",
+    motel: "Motel",
+    vacation_home: "Private vacation home",
+    aparthotel: "Aparthotel",
+    apartment: "Apartment",
+    condo_resort: "Condo resort",
+    villa: "Villa",
+    cottage: "Cottage",
+    guesthouse: "Guesthouse",
+    hostel: "Hostel/Backpacker accommodation",
+
+    // Property Brand
+    aloft: "aloft",
+    autograph_collection: "Autograph Collection",
+    best_western: "Best Western",
+    best_western_plus: "Best Western Plus",
+    boyd: "Boyd Gaming",
+    caesars: "Caesars Entertainment",
+    club_quarters: "Club Quarters",
+    conrad: "Conrad",
+    country_inn: "Country Inn & Suites - by Choice Hotels",
+    courtyard_marriott: "Courtyard",
+    crowne_plaza: "Crowne Plaza Hotels & Resorts",
+    curio_collection_hilton: "Curio Collection",
+    doubletree: "Doubletree",
+    embassy_suites: "Embassy Suites",
+    eurostars: "Eurostars Hotels",
+    extended_stay_america: "Extended Stay America Suites",
+    fairfield_inn: "Fairfield Inn",
+    four_seasons: "Four Seasons",
+    golden_nugget: "Golden Nugget",
+    hampton: "Hampton Inn",
+    hilton: "Hilton Hotels",
+    hilton_garden_inn: "Hilton Garden Inn",
+    hilton_grand: "Hilton Grand Vacations",
+    holiday_inn: "Holiday Inn",
+    holiday_inn_express: "Holiday Inn Express Hotel",
+    homewood_suites: "Homewood Suites by Hilton",
+    hyatt: "Hyatt Hotels",
+    hyatt_house: "Hyatt House",
+    hyatt_place: "Hyatt Place",
+    hyatt_regency: "Hyatt Regency",
+    intercontinental: "InterContinental Hotels & Resorts",
+    la_quinta: "La Quinta Inn & Suites",
+    loews: "Loews",
+    lxr_hotels_resorts: "LXR Hotels & Resorts",
+    marriott: "Marriott Hotels & Resorts",
+    mgm: "MGM",
+    motel6: "Motel 6",
+    oyo: "OYO AMER",
+    renaissance: "Renaissance",
+    sofitel: "Sofitel",
+    sonesta: "Sonesta Hotels",
+    springhill_suites: "SpringHill Suites",
+    station: "Station Casinos",
+    super8: "Super 8",
+    trump: "Trump Hotels",
+    travelodge: "Travelodge by Wyndham",
+    venetian_las_vegas: "Venetian Las Vegas",
+    westgate: "Westgate Resorts",
+    westin: "Westin",
+    world_bw: "World Hotels BW",
+    wynn: "Wynn Resorts",
+    wyndham_extra_holidays: "Wyndham Extra Holidays",
+
+    // Star Ratings
+    starRating_5: "5 stars",
+    starRating_4: "4 stars",
+    starRating_3: "3 stars",
+    starRating_2: "2 stars",
+    starRating_1: "1 star",
+
+    // Stay Option
+    stay_options_any: "Any",
+    stay_options_hotels: "Hotels",
+    stay_options_homes: "Homes",
+
+    // Traveler Experience
+    travel_experience_family_friendly: { name: "travelerType", aria_label: "Family friendly" },
+    travel_experience_pet_friendly: { name: "amenities", aria_label: "Pet friendly" },
+    travel_experience_adults_only: { name: "travelerType", aria_label: "Adults only" },
+    travel_experience_lgbtq: { name: "travelerType", aria_label: "LGBTQ welcoming" },
+    travel_experience_luxury: { name: "travelerType", aria_label: "Luxury" },
+    travel_experience_business: { name: "travelerType", aria_label: "Business friendly" },
+    travel_experience_beach: { name: "travelerType", aria_label: "Beach" },
+    travel_experience_romantic: { name: "travelerType", aria_label: "Romantic" },
+    travel_experience_eco: { name: "travelerType", aria_label: "Eco-certified" },
+    travel_experience_budget: { name: "travelerType", aria_label: "Budget" },
+    travel_experience_wedding: { name: "travelerType", aria_label: "Wedding" },
+
+    // Meals
+    meal_plan_breakfast: "Breakfast included",
+    meal_plan_dinner: "Dinner included",
+    meal_plan_lunch: "Lunch included",
+    meal_plan_all_inclusive: "All inclusive"
+};
+
+
+function applyFiltersInAgodaCom(smartFilters) {
+
+    for (const [key, value] of Object.entries(smartFilters)) {
+        if (key === "accessibility") {
+            value.forEach(accessibility => {
+                const mappedAccessibility = AGODA_COM_MAP[accessibility];
+                if (!mappedAccessibility) return;
+                let checkbox = document.querySelector(`input[name='accessibility'][aria-label*='${mappedAccessibility}']`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "propertyAccessibility") {
+            value.forEach(accessibility => {
+                const mappedAccessibility = AGODA_COM_MAP[accessibility];
+                if (!mappedAccessibility) return;
+                let checkbox = document.querySelector(`input[name='accessibility'][aria-label*='${mappedAccessibility}']`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "propertyAmenities") {
+            value.forEach(amenity => {
+                const mappedAmenity = AGODA_COM_MAP[amenity];
+                if (!mappedAmenity) return;
+                const checkbox = document.querySelector(`input[name="amenities"][aria-label*="${mappedAmenity}"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "roomAmenities") {
+            value.forEach(amenity => {
+                const mappedAmenity = AGODA_COM_MAP[amenity];
+                if (!mappedAmenity) return;
+                const checkbox = document.querySelector(`input[name="amenities"][aria-label*="${mappedAmenity}"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "availability") {
+            value.forEach(availability => {
+                const mappedAvailability = AGODA_COM_MAP[availability];
+                if (!mappedAvailability) return;
+                const checkbox = document.querySelector(`input[name="availableFilter"][aria-label*="${mappedAvailability}"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "cancellation") {
+            value.forEach(cancellation => {
+                const mappedCancellation = AGODA_COM_MAP[cancellation];
+                if (!mappedCancellation) return;
+                const checkbox = document.querySelector(`input[name="paymentType"][aria-label*="${mappedCancellation}"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "discounts") {
+            value.forEach(discount => {
+                const mappedDiscount = AGODA_COM_MAP[discount];
+                if (!mappedDiscount) return;
+                const checkbox = document.querySelector(`input[name="rewards"][aria-label*="${mappedDiscount}"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "guestRatings") {
+            if (value.length === 0) {
+                const checkbox = document.querySelector(`input[name="guestRating"][aria-label*="guest_rating_any"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+                continue;
+            }
+            const minRating = value.reduce((min, current) => {
+                const currentNum = parseInt(current.split('_').pop(), 10);
+                const minNum = parseInt(min.split('_').pop(), 10);
+                return currentNum < minNum ? current : min;
+            });
+
+            const mappedGuestRating = AGODA_COM_MAP[minRating];
+            let checkbox;
+            if (!mappedGuestRating) {
+                checkbox = document.querySelector(`input[name="guestRating"][aria-label*="guest_rating_any"]`);
+
+            }
+            else {
+                checkbox = document.querySelector(`input[name="guestRating"][aria-label*="${mappedGuestRating}"]`);
+            }
+            if (checkbox && !checkbox.checked) {
+                checkbox.click();
+            }
+        }
+        // handle minPrice and maxPrice together
+        else if (key === "maxPrice") {
+            const sliderMax = document.querySelector('input[type="range"][aria-label*="Maximum"]');
+            const sliderMin = document.querySelector('input[type="range"][aria-label*="Minimum"]');
+            if (sliderMax && value != null) {
+                sliderMax.value = value;
+                // don't try to put MouseDown event, it will trigger sliderMax not work. 
+                // I guess mousedown take some time to process, then sliderMax.mouseup event will be too closed to sliderMin.mouseup
+                // then only one mouseup event is executed.
+                sliderMax.dispatchEvent(new MouseEvent('mouseup', { bubbles: true }));
+            }
+
+            if (sliderMin && smartFilters.minPrice != null) {
+                setTimeout(() => {
+                    sliderMin.value = smartFilters.minPrice || 0;
+                    sliderMin.dispatchEvent(new MouseEvent('mouseup', { bubbles: true }));
+                }, 2000);
+            }
+        }
+        else if (key === "meals") {
+            value.forEach(meal => {
+                const mappedMeal = AGODA_COM_MAP[meal];
+                if (!mappedMeal) return;
+                const checkbox = document.querySelector(`input[name="mealPlan"][aria-label*="${mappedMeal}"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "paymentFlexibility") {
+            value.forEach(paymentFlexibility => {
+                const mappedPaymentFlexibility = AGODA_COM_MAP[paymentFlexibility];
+                if (!mappedPaymentFlexibility) return;
+                const checkbox = document.querySelector(`input[name="paymentType"][aria-label*="${mappedPaymentFlexibility}"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "propertyBrands") {
+            value.forEach(brand => {
+                const mappedBrand = AGODA_COM_MAP[brand];
+                if (!mappedBrand) return;
+                if (mappedBrand === "Holiday Inn") {
+                    const checkboxes = document.querySelectorAll(`input[name="hotel_brand"][aria-label*="${mappedBrand}"]`);
+                    for (const checkbox of checkboxes) {
+                        const label = checkbox.getAttribute("aria-label") || "";
+                        if (!label.startsWith("Holiday Inn Express")) {
+                            if (!checkbox.checked) {
+                                checkbox.click();
+                            }
+                            break;
+                        }
+                    }
+                }
+                else {
+                    const checkbox = document.querySelector(`input[name="hotel_brand"][aria-label*="${mappedBrand}"]`);
+                    if (checkbox && !checkbox.checked) {
+                        checkbox.click();
+                    }
+                }
+            });
+        }
+        else if (key === "propertyTypes") {
+            value.forEach(type => {
+                const mappedType = AGODA_COM_MAP[type];
+                if (!mappedType) return;
+                const checkbox = document.querySelector(`input[name="lodging"][aria-label*="${mappedType}"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "starRatings") {
+            value.forEach(rating => {
+                const mappedRating = AGODA_COM_MAP[rating];
+                if (!mappedRating) return;
+                const checkbox = document.querySelector(`input[name="star"][aria-label*="${mappedRating}"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+        else if (key === "stayOption") {
+            const mappedValue = AGODA_COM_MAP[value];
+            if (mappedValue) {
+                const radio = document.querySelector(`input[name="stay_options_group"][aria-label*="${mappedValue}"]`);
+                if (radio && !radio.checked) {
+                    radio.click(); // Simulate real user interaction
+                }
+            }
+        }
+        else if (key === "travelerExperiences") {
+            value.forEach(experience => {
+                const mappedExperience = AGODA_COM_MAP[experience];
+                if (!mappedExperience) return;
+                const checkbox = document.querySelector(`input[name="${mappedExperience.name}"][aria-label*="${mappedExperience.aria_label}"]`);
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
+            });
+        }
+    }
 
 }
